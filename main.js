8b5e7c92323bfb13288913904986aef644e83eeb
Sephtonlol <132364058+Sephtonlol@users.noreply.github.com>
2023-11-01 23:38:40 +0100

    commit

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
-// This function is automatically called whenever the content of the search box changes.
+// als de zoekbalk leeg is zal de Iframe automatisch sluiten
 document.getElementById("searchBox").addEventListener("input", function() {
     var query = document.getElementById("searchBox").value;
     var searchUrl1 = "https://support.microsoft.com/search/results?query=" + encodeURIComponent(query);
+    var searchFrame = document.getElementById("searchFrame");
     if (query.trim() === "") {
-        // If the search input is empty, close the search container by setting iframe's src to an empty string.
+        // "if" zal die sluiten
         document.getElementById("searchFrame").src = "";
     } else {
-        // If there's a search query, update the search container with the search results.
+        // "else" zal die open blijven
         document.getElementById("searchFrame").src = searchUrl1;
+        // dit zorgt ervoor dazt scrollen werkt met gebruik van deze source in de Iframe
+        searchFrame.removeAttribute("scrolling");
+        searchFrame.setAttribute("scrolling", "yes");
     }
 });
 
 var button4 = document.getElementById("button4");
-var searchUrl2 = "https://chat.openai.com/";
-// Voeg een event listener toe aan de knop
+var searchUrl2 = "https://chat.openai.com/?model=text-davinci-002-render-sha";
 button4.addEventListener("click", function() {
-// Wanneer de knop wordt ingedrukt, maak de bron van het iframe leeg
+// Wanneer de knop wordt ingedrukt zal de zoekbalk automatisch leeg gemaakt worden
 document.getElementById("searchBox").value = "";
-    // Maak ook de bron van het iframe leeg
+    // opent een website in de Iframe
 document.getElementById("searchFrame").src = searchUrl2;
 });
 var button7 = document.getElementById("button7");
-var searchUrl3 = "http://www.staggeringbeauty.com/";
+var searchUrl3 = "https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx&itrid=654275aed15a494093ecbc9394a1f932";
+var searchFrame = document.getElementById("searchFrame");
 // Voeg een event listener toe aan de knop
 button7.addEventListener("click", function() {
-// Wanneer de knop wordt ingedrukt, maak de bron van het iframe leeg
+// Wanneer de knop wordt ingedrukt zal de zoekbalk automatisch leeg gemaakt worden
 document.getElementById("searchBox").value = "";
-    // Maak ook de bron van het iframe leeg
+    // opent een website in de Iframe
 document.getElementById("searchFrame").src = searchUrl3;
+searchFrame.removeAttribute("scrolling");
+searchFrame.setAttribute("scrolling", "no");
 });
 
 var icon = document.getElementById("icon");
@@ -35,3 +41,24 @@ var icon = document.getElementById("icon");
 icon.onclick= function(){
     document.body.classList.toggle("dark-theme");
 };
+
+const button = document.getElementById('mountainGoats');
+const mountainGoatsByClass = document.getElementsByClassName("play");
+const audio = new Audio("./goatScream.mp3");
+
+let clickCount = 0;
+let lastClickTime = 0;
+button.addEventListener('click', () => {
+  const currentTime = new Date().getTime();
+//   telt alleen clicks binnen de 2 seconden time frame
+  if (currentTime - lastClickTime > 2000) {
+    clickCount = 0;
+  }
+  clickCount++;
+  lastClickTime = currentTime;
+  if (clickCount === 7) {
+    audio.play();
+    // reset de click counter na het bereiken van de 7 clicks
+    clickCount = 0; 
+  }
+});
